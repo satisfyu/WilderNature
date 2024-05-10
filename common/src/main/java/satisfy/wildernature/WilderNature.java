93e3e3db6a0f262cf3beb092be21ab22a753c1e6
jason13official <jason13official@gmail.com>
2024-05-10 18:06:31 -0400

    feat: mixins, begin truffling

--- common/src/main/java/satisfy/wildernature/WilderNature.java
+++ common/src/main/java/satisfy/wildernature/WilderNature.java
@@ -1,11 +1,13 @@
 package satisfy.wildernature;
 
+import dev.architectury.event.events.common.InteractionEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import satisfy.wildernature.registry.EntityRegistry;
 import satisfy.wildernature.registry.ObjectRegistry;
 import satisfy.wildernature.registry.SoundRegistry;
 import satisfy.wildernature.registry.TabRegistry;
+import satisfy.wildernature.util.Truffling;
 import satisfy.wildernature.util.WilderNatureProperties;
 
 public class WilderNature {
@@ -21,6 +23,7 @@ public class WilderNature {
     }
 
     public static void commonInit() {
+        // InteractionEvent.RIGHT_CLICK_ITEM.register(Truffling::onPlayerUseEat); // same as: (player, hand) -> Truffling.onPlayerUseItem(player, hand)
     }
 }
 
---
+++ forge/src/main/java/satisfy/wildernature/forge/mixin/FoodDataMixin.java
@@ -0,0 +1,42 @@
+package satisfy.wildernature.forge.mixin;
+
+//import io.github.mortuusars.salt.Salt;
+//import io.github.mortuusars.salt.Salting;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.food.FoodData;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import satisfy.wildernature.util.Truffling;
+
+@Mixin(FoodData.class)
+public class FoodDataMixin {
+    @Shadow
+    public void eat(int foodLevel, float saturationModifier) { }
+
+    @Inject(method = "eat(Lnet/minecraft/world/item/Item;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/LivingEntity;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/food/FoodData;eat(IF)V"), cancellable = true)
+    private void onEat(Item pItem, ItemStack pStack, LivingEntity entity, CallbackInfo ci) {
+        if (!Truffling.isTruffled(pStack))
+            return;
+
+        FoodProperties foodProperties = pStack.getFoodProperties(entity);
+        Truffling.FoodValue additionalFoodValues = Truffling.getAdditionalFoodValue(pStack);
+
+        eat(foodProperties.getNutrition() + additionalFoodValues.nutrition(),
+                foodProperties.getSaturationModifier() + additionalFoodValues.saturationModifier());
+
+        // if (entity instanceof ServerPlayer serverPlayer && !serverPlayer.isCreative())
+        //     Salt.Advancements.SALTED_FOOD_CONSUMED.trigger(serverPlayer);
+
+        ci.cancel();
+    }
+}
+
