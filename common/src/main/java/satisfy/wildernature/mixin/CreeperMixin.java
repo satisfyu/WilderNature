751d2b90f12a9617aa92bc35e94a8c1a587c2f45
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-05-28 11:01:05 +0200

    Added: Trinkets support

--- common/src/main/java/satisfy/wildernature/mixin/CreeperMixin.java
+++ common/src/main/java/satisfy/wildernature/mixin/CreeperMixin.java
@@ -5,11 +5,11 @@ import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.EntitySelector;
 import org.spongepowered.asm.mixin.Unique;
-import satisfy.wildernature.item.FurCloakItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import satisfy.wildernature.item.FurCloakItem;
 
 @Mixin(Creeper.class)
 public abstract class CreeperMixin {
@@ -21,7 +21,8 @@ public abstract class CreeperMixin {
     private void onEntityTick(CallbackInfo ci) {
         Creeper creeper = (Creeper) (Object) this;
         if (!wilderNature$addedGoal) {
-            ((MobAccessor) creeper).getGoalSelector().addGoal(3, new AvoidEntityGoal<>(creeper, Player.class, 6.0F, 1.0, 1.2, target -> target != null && FurCloakItem.isEquippedBy((Player) target) && EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(target)));
+            ((MobAccessor) creeper).getGoalSelector().addGoal(3, new AvoidEntityGoal<>(creeper, Player.class, 6.0F, 1.0, 1.2, target ->
+                    target != null && FurCloakItem.isEquippedBy((Player) target) && EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(target)));
             wilderNature$addedGoal = true;
         }
     }
