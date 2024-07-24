4cf4d83d4f65b95e7bd016ddc2f5b6a68c4ec8a2
ipz233_vvv <jumpergooog@gmail.com>
2024-07-24 20:46:38 +0300

    fixed attack animation

--- common/src/main/java/net/satisfy/wildernature/entity/ai/BisonAttackGoal.java
+++ common/src/main/java/net/satisfy/wildernature/entity/ai/BisonAttackGoal.java
@@ -4,11 +4,12 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.satisfy.wildernature.entity.BisonEntity;
+import net.satisfy.wildernature.entity.animation.BisonAnimation;
 
 public class BisonAttackGoal extends MeleeAttackGoal {
     private final BisonEntity bison;
     private int counter;
-    private int attackDelay = 40;
+    private int attackDelay = (int) (BisonAnimation.attack.lengthInSeconds()*20)+5;
     private int attackFrame = 5;
 
     public BisonAttackGoal(BisonEntity pMob, double pSpeedModifier, boolean pFollowingTargetEvenIfNotSeen) {
@@ -22,21 +23,31 @@ public class BisonAttackGoal extends MeleeAttackGoal {
         if(target!=null){
             checkAndPerformAttack(target,bison.getMeleeAttackRangeSqr(target));
         }
+        bison.setAttacking(counter!=0);
+
+        if(counter!=0)
+            counter++;
+        if(counter>=attackDelay){
+            counter=0;
+        }
     }
 
     @Override
     protected void checkAndPerformAttack(LivingEntity targetEntity, double discanceSqr) {
         if (targetEntity.getPosition(0).distanceToSqr(bison.getPosition(0))<discanceSqr) {
+            counter++;
             if (counter == attackFrame) {
                 this.bison.doHurtTarget(targetEntity);
             }
-            counter++;
         }
 
-        if(counter>=attackDelay){
-            counter=0;
-        }
 
-        bison.setAttacking(counter!=0);
+
+    }
+
+    @Override
+    public void stop() {
+        bison.setAttacking(false);
+        super.stop();
     }
 }
